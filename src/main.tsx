92711001882e1c83444e3f11bc7da22ab900ae10
Willian Almeida Alves <willian.alvesb65@gmail.com>
2025-07-19 22:35:21 -0300

    fix: remove import main css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import "@/shared/styles/style.css";
 import App from "./App.tsx";
+import "@/shared/styles/style.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
