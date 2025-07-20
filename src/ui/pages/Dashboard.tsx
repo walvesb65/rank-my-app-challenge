2da3d9edc8ef4d361b90a8db0776354b301ef623
Willian Almeida Alves <willian.alvesb65@gmail.com>
2025-07-19 22:36:19 -0300

    feat: label para itnes da Dashboard

--- src/ui/pages/Dashboard.tsx
+++ src/ui/pages/Dashboard.tsx
@@ -2,26 +2,23 @@ import { useAppList } from "@/ui/hooks/useAppList";
 import { AppForm } from "@/presentation/components/AppForm";
 import AppList from "@/presentation/components/AppList";
 import AppFilterForm from "@/presentation/components/AppFilterForm";
-import { Typography, Divider } from "antd";
 
 const Dashboard = () => {
   const { apps, loading, reload } = useAppList();
 
   return (
-    <div className="max-w-4xl mx-auto space-y-6 p-4">
-      <Typography.Title level={3}>Cadastrar Novo Aplicativo</Typography.Title>
+    <div className="max-w-4xl mx-auto space-y-6">
+      <h2 className="text-2xl font-bold">Cadastrar Aplicativo</h2>
       <AppForm onAppCreated={reload} />
 
-      <Divider />
-
-      <Typography.Title level={3}>Aplicativos Cadastrados</Typography.Title>
-
+      <h2 className="text-2xl font-bold">Filtrar Aplicativos</h2>
       <AppFilterForm />
 
+      <h2 className="text-2xl font-bold">Aplicativos Cadastrados</h2>
       {loading ? (
-        <p>Carregando aplicativos...</p>
+        <p className="text-gray-500">Carregando apps...</p>
       ) : apps.length === 0 ? (
-        <p>Nenhum app encontrado.</p>
+        <p className="text-gray-400">Nenhum aplicativo encontrado.</p>
       ) : (
         <AppList apps={apps} />
       )}
