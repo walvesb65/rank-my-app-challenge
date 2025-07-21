246235c3cafdd282b181fb1cae2d62437b49492c
Willian Almeida Alves <willian.alvesb65@gmail.com>
2025-07-21 15:52:34 -0300

    fix: caminho img logo

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -7,11 +7,7 @@ const AppHeader = () => {
     <Header className="bg-[#1A88FF] shadow-sm sticky top-0 z-50 px-6 flex items-center justify-between h-16">
       <div className="flex items-center gap-2">
         <a href="/">
-          <img
-            src="public/Logo-RankMyApp.svg"
-            className=" h-8"
-            alt="logo RankMyApp"
-          />
+          <img src="/Logo-RankMyApp.svg" className="h-8" alt="logo RankMyApp" />
         </a>
       </div>
     </Header>
